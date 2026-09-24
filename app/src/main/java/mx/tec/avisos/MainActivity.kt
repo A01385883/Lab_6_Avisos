2339f35873c025a928da79b8cb93a01404679a6c
A01385883 <a01385883@tec.mx>
2026-09-23 20:37:48 -0600

    Realmente es solo una linea? (FLAG.SECURE)

--- app/src/main/java/mx/tec/avisos/MainActivity.kt
+++ app/src/main/java/mx/tec/avisos/MainActivity.kt
@@ -1,6 +1,7 @@
 package mx.tec.avisos
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import mx.tec.avisos.ui.navigation.AvisosApp
@@ -9,6 +10,9 @@ import mx.tec.avisos.ui.theme.AvisosTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Realmente nomas es esto?
+        //Realmente solio es eso
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
         setContent { AvisosTheme { AvisosApp() } }
     }
 }
